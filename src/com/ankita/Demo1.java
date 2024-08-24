08d5d3823d4041c904c1615c1bd9c957c6ed8dbf
Ankita Padole <ankitapadole111@gmail.com>
2024-08-24 08:57:10 +0530

    my first git commit

---
+++ src/com/ankita/Demo1.java
@@ -0,0 +1,11 @@
+package com.ankita;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+	System.out.println("Hello all");
+	System.out.println("welcome to git");
+
+	}
+
+}
---
+++ src/com/ankita/Demo2.java
@@ -0,0 +1,27 @@
+package com.ankita;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo2 {
+	public static void m1()
+	{
+		List<String>list=new ArrayList<String>();
+		list.add("core java");
+		list.add("oops concept");
+		list.add("hybernate");
+		list.add("jdbc");
+		list.add("git");
+		for(String str:list)
+		{
+			System.out.println(str);
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		m1();
+
+	}
+
+}
